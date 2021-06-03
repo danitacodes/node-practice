b97f68f29e4371377f980f0638b3b1fb4b1a7bff
Danita Forbes <danitacodes@gmail.com>
2021-06-02 18:21:19 -0700

    Created app js and ejs files

--- app.js
+++ app.js
@@ -4,23 +4,30 @@ const express = require('express');
 
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Why I started learning code', snippet: 'I started learning'},
+        {title: 'What Have I Learned So Far', snippet: 'I have learned'},
+        {title: 'What Projects I Have Done', snippet: 'My first project was'},
+    ];
 
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
-//redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('./about');
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create New Blog' });
+})
 
 //404
 app.use((req, res) => {
-    res.sendFile('./views/404.html', {root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
