b45f212840e342e904034cb5dffeb41233cc037d
Danita Forbes <danitacodes@gmail.com>
2021-06-04 11:09:14 -0700

    added mongodb

--- app.js
+++ app.js
@@ -1,31 +1,98 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //express app
 
 const app = express();
 
+//connect to MongoDB
+const dbURI = 'mongodb+srv://danita:1luv4all@blog.ziibm.mongodb.net/Blog?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser:true, useUnifiedTopology:true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000);
+//middleware and static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
 
+app.get('/add-blog', (req, res) =>{
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('60b9a56fcc058a02bcc8e671')
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+//routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Why I started learning code', snippet: 'I started learning'},
-        {title: 'What Have I Learned So Far', snippet: 'I have learned'},
-        {title: 'What Projects I Have Done', snippet: 'My first project was'},
-    ];
-
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
+//blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+     res.render('index', {title: 'All Blogs', blogs: result })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog({req.body});
+
+    blog.save()
+    .then((result) =>{
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create New Blog' });
 })
+
 
 //404
 app.use((req, res) => {
